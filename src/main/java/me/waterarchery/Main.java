7b6155112e4fa8dbb43f1888af489404ef821401
WaterArchery <loljoket@gmail.com>
2024-04-22 18:55:51 +0300

    Created a simple request sender

---
+++ src/main/java/me/waterarchery/Main.java
@@ -0,0 +1,41 @@
+package me.waterarchery;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        sendRequest();
+    }
+
+    private static void sendRequest() {
+        try {
+            // Preparing connection
+            URL url = new URL("http://universities.hipolabs.com/search?name=bahce");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            InputStream is = con.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder();
+
+            // Parsing response
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+
+            // Closing reader and printing response
+            rd.close();
+            System.out.println(response);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
